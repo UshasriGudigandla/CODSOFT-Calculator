9926d0505469670c3bf608054dc7030ac67c0eac
UshasriGudigandla <gudigandlaushasri12345@gmail.com>
2025-01-04 18:37:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
+
+function deleteDisplay(){
+    const display=document.getElementById('display');
+    display.value = display.value.toString().slice(0,-1);
+}
